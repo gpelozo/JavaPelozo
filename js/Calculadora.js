dada7e5b9a5473722e9aad4f36544bf61044d316
cepoid <gpelozo@cepoid.com>
2022-09-04 14:51:25 -0300

    JSON

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -70,6 +70,22 @@ function showAlert () {
   .then( (data) => {
       console.log(data)
   })
+//Obtener mi perfil JSON de GitHub
+function exito() {
+    var datos = JSON.parse(this.responseText); //convertir a JSON
+    console.log(datos);
+}
+
+// funcion para la llamada fallida
+function error(err) {
+    console.log('Solicitud fallida', err); //los detalles en el objecto "err"
+}
+
+var xhr = new XMLHttpRequest(); //invocar nueva instancia de XMLHttpRequest
+xhr.onload = exito; // llamar a la funcion exito si exitosa
+xhr.onerror = error;  // llamar a la funcion error si fallida
+xhr.open('GET', 'https://api.github.com/users/gpelozo'); // Abrir solicitud GET
+xhr.send(); // mandar la solicitud al servidor.
 
 //Evento click
 btn1.addEventListener('click', showAlert);
