75b191a9768e5d4256749fe7baa1d8c7a1820765
cepoid <gpelozo@cepoid.com>
2022-09-23 23:43:54 -0300

    Carrito Lleno

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -120,20 +120,39 @@ btn1.addEventListener('click', solicitarDato);
 compra1.addEventListener('click', showAlert);
 compra2.addEventListener('click', showAlert);
 compra3.addEventListener('click', showAlert);
-//////////
 
+
+
+
+
+//////////CARRITO DE COMPRAS DE DIVISAS
+
+const cards = document.getElementById('cards')
 const items = document.getElementById('items')
+const footer = document.getElementById('footer')
 const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
 const fragment = document.createDocumentFragment
 let carrito = {}
-//cargar una vez cargada la pagina
+//Eventos
+//cargar una vez cargada la pagina las sesion anterior
 document.addEventListener('DOMContentLoaded', () => {
   fetchData()
+  if(localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    pintarCarrito()
+  }
 })
-items.addEventListener('click', e => {
 
+cards.addEventListener('click', e => {
+  addCarrito(e)
+});
+
+items.addEventListener('click', e => {
+  btnAccion(e)
 })
-//
+//Traer Divisas
 const fetchData = async() => {
   try {
     const res = await fetch('api.json')
@@ -156,16 +175,13 @@ const pintarCards = data => {
     const clone = templateCard.cloneNode(true)
     fragment.appendChild(clone)
   })
-  items.appendChild(fragment)
+  cards.appendChild(fragment)
 }
 
 //AgregarCarrito
 const addCarrito = e => {
-  console.log(e.target)
-  console.log(e.target.classList.contains('btn-primary'))
   if (e.target.classList.contains('btn-primary')) {
     setCarrito(e.target.parentElement)
-
   }
   e.stopPropagation()
 }
@@ -182,13 +198,54 @@ if (carrito.hasOwnProperty(producto.id)){
 producto.cantidad = carrito[producto.id].cantidad + 1
 }
 carrito[producto.id] = {...producto}
-console.log(carrito)
+pintarCarrito()
 }
-//Carrito
-let Ejcarrito = {
-  1: { nombre: "item 1"},
-  2: { nombre: "item 2"},
-};
+//cambiar informacion carrito
+const pintarCarrito = () => {
+  console.log(carrito)
+  items.innerHTML= ''
+  Object.values(carrito).forEach(producto => {
+    templateCarrito.querySelector('th').textContent = producto.id
+    templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+    templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+    templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+    templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+    templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+    const clone = templateCarrito.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+  items.appendChild(fragment)
+  pintarFooter()
+
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+//cambiarLeyenda
+const pintarFooter = () => {
+  footer.innerHTML = ''
+  if (Object.keys(carrito).length === 0) {
+    footer.innerHTML = `
+    <th scope="row" colspan="5"> Carrito de Rol Argentino vacío - comience a comprar! </th>
+    `
+    return
+  }
+//sumar total
+  const nCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad,0)
+  const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio,0)
+
+  templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+  templateFooter.querySelector('span')[0].textContent = nPrecio
+  
+  const clone = templateFooter.cloneNode(true)
+  fragment.appendChild(clone)
+  footer.appendChild(fragment)
+//Vaciar carrito
+  const btnVaciar = document.getElementById('vaciar-carrito')
+  btnVaciar.addEventListener('click', () => {
+    carrito = {}
+    pintarCarrito()
+  })
+}
 
 //guardarCarrito
 document.addEventListener('DOMContentLoaded'), () => {
@@ -198,8 +255,32 @@ document.addEventListener('DOMContentLoaded'), () => {
     pintarCarrito()
   }
 }
+//Botones de forma dinamica con delegacion de evento
+const btnAccion = e => {
+  console.log(e.target)
+  //aumentar
+  if (e.target.classList.contains('btn-info')) {
+    const producto = carrito[e.target.dataset.id]
+    producto.cantidad++
+    carrito[e.target.dataset.id] = {...producto }
+    pintarCarrito()
+  }
+//reducir
+  if (e.target.classList.contains('btn-danger')) {
+    const producto = carrito[e.target.dataset.id]
+    producto.cantidad--
+    if (producto.cantidad === 0) {
+      delete carrito[e.target.dataset.id]
+      pintarCarrito()
+    }
+  }
 
-//calculadora
+  e.stopPropagation()
+}
+
+
+
+// ////////CALCULADORA AUXILIAR
 let display4 = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
 
